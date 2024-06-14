aeda29dae6e01c0ab5082ea59dd86d2a0cc69c0c
akshad07 <akshadpardeshi07@gmail.com>
2024-06-14 23:18:49 +0530

    Views.py

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import WeatherData
+from .serializers import WeatherDataSerializer
+from .utils import fetch_weather_data
 
-# Create your views here.
+class WeatherDataListCreate(generics.ListCreateAPIView):
+    queryset = WeatherData.objects.all()
+    serializer_class = WeatherDataSerializer
+
+    def perform_create(self, serializer):
+        url = self.request.data.get('url')
+        weather_data = fetch_weather_data(url)
+        for data in weather_data:
+            WeatherData.objects.create(**data)
